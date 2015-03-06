7ced38cf91e5379d0237d67aab3ff907cd58351a
Romeu Fonseca <romeu.hcf@gmail.com>
2015-03-06 00:50:24 -0300

    Less dependencies

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,8 +4,8 @@
 # loaded once.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
-require 'simplecov'
-SimpleCov.start
+#require 'simplecov'
+#SimpleCov.start
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
@@ -16,5 +16,5 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = 'random'
-  
+
 end
